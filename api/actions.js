cf6517e0c6add8055fd2710dfdc79c6d063dc540
Erin Meredith <ekmeredith@att.net>
2018-07-27 12:37:13 -0400

    GET requests functioning

--- api/actions.js
+++ api/actions.js
@@ -6,5 +6,26 @@ const router = express.Router();
 server.use(express.json());
 
 
+router.get('/', async (req, res) => {
+    try {
+        const actions = await actionDb.get();
+        res.status(200).json(actions)
+    } catch(err) {
+        res.status(500).json({error: 'The actions information could not be retrieved.'})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const actions = await actionDb.get(id);
+        res.status(200).json(actions);
+        if (!id) {
+            res.status(404).json({error: 'The action with the specified id does not exist.'})
+        }
+    } catch(err) {
+        res.status(500).json({error: 'The action information could not be retrieved.'})
+    }
+})
 
 module.exports = router;
--- api/projects.js
+++ api/projects.js
@@ -14,6 +14,18 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const projects = await projectDb.get(id);
+        res.status(200).json(projects);
+        if (!id) {
+            res.status(404).json({error: 'The project with the specified id does not exist.'})
+        }
+    } catch(err) {
+        res.status(500).json({error: 'The project information could not be retrieved.'})
+    }
+})
 
 
 module.exports = router;
